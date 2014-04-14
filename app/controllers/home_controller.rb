8611f85b611974a0fa5c08b00771ca5662b5d32e
Sam Baumgarten <sambaumgarten98@gmail.com>
2014-04-14 02:07:06 -0700

    Update algorithm and item count

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,19 +4,19 @@ class HomeController < ApplicationController
   
   def autocomplete
     results = []
-    Drawer.search(params[:q]).each do |result|
-      r = {}
-      r[:_id] = result["_id"]
-      r[:label] = result[:_source][:label]
-      r[:model] = "drawer"
-      results << r
-    end
-    
     Item.search(params[:q]).each do |result|
       r = {}
       r[:_id] = result["_id"]
       r[:label] = result[:_source][:name]
       r[:model] = "item"
+      results << r
+    end
+    
+    Drawer.search(params[:q]).each do |result|
+      r = {}
+      r[:_id] = result["_id"]
+      r[:label] = result[:_source][:label]
+      r[:model] = "drawer"
       results << r
     end
     render :json => results
