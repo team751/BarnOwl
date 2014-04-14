ee87b975e7453110f31f42dc2650af746a53a40a
Sam Baumgarten <sambaumgarten98@gmail.com>
2014-04-14 01:37:22 -0700

    update to bonsai

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -2,8 +2,8 @@ require 'elasticsearch/model'
 require 'elasticsearch/transport'
 
 if Rails.env.production?
-  Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
-  Item.__elasticsearch__.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
-  Drawer.__elasticsearch__.client = Elasticsearch::Client.new host: ENV['SEARCHBOX_URL']
+  Elasticsearch::Client.new host: ENV['BONSAI_URL']
+  Item.__elasticsearch__.client = Elasticsearch::Client.new host: ENV['BONSAI_URL']
+  Drawer.__elasticsearch__.client = Elasticsearch::Client.new host: ENV['BONSAI_URL']
   # Elasticsearch::Model.client = Elasticsearch::Client.new url: "http://paas:104cd4f8914496912532b33476089450@dwalin-us-east-1.searchly.com"
 end
