df46265231c4af4de2adbef4539591be2161d2ef
Sam Baumgarten <sambaumgarten98@gmail.com>
2014-04-16 16:37:55 -0700

    remove drawers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,13 +12,13 @@ class HomeController < ApplicationController
       results << r
     end
     
-    Drawer.search(params[:q]).each do |result|
-      r = {}
-      r[:_id] = result["_id"]
-      r[:label] = result[:_source][:label]
-      r[:model] = "drawer"
-      results << r
-    end
+    # Drawer.search(params[:q]).each do |result|
+    #   r = {}
+    #   r[:_id] = result["_id"]
+    #   r[:label] = result[:_source][:label]
+    #   r[:model] = "drawer"
+    #   results << r
+    # end
     render :json => results
   end
 end
