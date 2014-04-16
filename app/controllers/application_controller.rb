94e67e5a7eeee51b51f9aaf3e8e5492e9f420dc3
Sam Baumgarten <sambaumgarten98@gmail.com>
2014-04-16 02:43:11 -0700

    remove warning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ class ApplicationController < ActionController::Base
   private
 
   def check_registration
-    if current_user_if_exists && !current_user_if_exists.valid?
-      flash[:warning] = "Please finish your #{view_context.link_to "registration", edit_user_registration_url }  before continuing.".html_safe
+    if current_user_if_exists && !current_user.valid?
+      # flash[:warning] = "Please finish your #{view_context.link_to "registration", edit_user_registration_url }  before continuing.".html_safe
     end
   end
 
