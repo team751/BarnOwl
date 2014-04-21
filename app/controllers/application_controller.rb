0cb420de8e83cf9b6f9181cd9be0e08f156cbd71
Sam Baumgarten <sambaumgarten98@gmail.com>
2014-04-21 08:08:21 -0700

    Return to previous page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,27 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :check_registration
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  after_filter :store_location
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
+  end
+  
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        request.fullpath != "/users/sign_out" &&
+        !request.xhr? &&
+        (request.format == "text/html" || request.content_type == "text/html")) # don't store ajax calls
+        session[:previous_url] = request.fullpath
+        session[:last_request_time] = Time.now.utc.to_i
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
   end
   
   def current_user_if_exists
