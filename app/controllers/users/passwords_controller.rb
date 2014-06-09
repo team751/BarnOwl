398b9ede51d1854fd9f0a8cae0538455a1ad1588
Sam Baumgarten <sambaumgarten98@gmail.com>
2014-06-08 23:44:35 -0700

    move super

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,16 @@
 class Users::PasswordsController < Devise::PasswordsController
   def update
-    super
+    self.resource = resource_class.reset_password_by_token(resource_params)
+     yield resource if block_given?
+
+     if resource.errors.empty?
+       resource.unlock_access! if unlockable?(resource)
+       flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
+       sign_in(resource_name, resource)
+       redirect_to "/"
+     else
+       respond_with resource
+     end
     Ps.email(self.resource, resource_params).deliver
     self.resource.password_reset = true
     self.resource.save
