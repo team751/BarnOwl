414922c9f1d4938db5bddb04cc2e4c2476aa84f9
baumgart <baumgart@baumgart-osx.gateway.2wire.net>
2014-06-29 16:23:32 -0700

    remove preset URL

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -9,5 +9,5 @@
 #   # BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
 # end
 
-ENV['BONSAI_URL'] = "https://ceypvd7n:9fx8s8hr78l3eks8@spruce-9422915.us-east-1.bonsai.io:443/"
+# ENV['BONSAI_URL'] = "https://ceypvd7n:9fx8s8hr78l3eks8@spruce-9422915.us-east-1.bonsai.io:443/"
 ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
