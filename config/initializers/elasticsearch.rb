69f8a4ef737137dd32ba98f32d1f915176734c01
baumgart <baumgart@baumgart-osx.gateway.2wire.net>
2014-06-29 16:27:26 -0700

    fix es config

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,13 +1,12 @@
-# require 'elasticsearch/model'
-# 
-# if ENV['BONSAI_URL']
-#   Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
-#   BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
-# else
-#   Elasticsearch::Model.client = Elasticsearch::Client.new({url: "localhost:9200", logs: true})
-#   # app_name = Rails.application.class.parent_name.underscore.dasherize
-#   # BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
-# end
+require 'elasticsearch/model'
 
-# ENV['BONSAI_URL'] = "https://ceypvd7n:9fx8s8hr78l3eks8@spruce-9422915.us-east-1.bonsai.io:443/"
-ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
+if ENV['BONSAI_URL']
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
+  BONSAI_INDEX_NAME = ENV['BONSAI_URL'][/[^\/]+$/]
+else
+  Elasticsearch::Model.client = Elasticsearch::Client.new({url: "localhost:9200", logs: true})
+  # app_name = Rails.application.class.parent_name.underscore.dasherize
+  # BONSAI_INDEX_NAME = "#{app_name}-#{Rails.env}"
+end
+
+# ENV['ELASTICSEARCH_URL'] = ENV['BONSAI_URL']
